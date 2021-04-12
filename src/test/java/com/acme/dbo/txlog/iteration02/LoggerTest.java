ece2be354228e709c3aea945e236e500cfc3e673
vadim <dulubvadim@gmail.com>
2021-04-12 21:50:04 +0300

    Change static to classes

--- src/test/java/com/acme/dbo/txlog/iteration02/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration02/LoggerTest.java
@@ -2,6 +2,7 @@ package com.acme.dbo.txlog.iteration02;
 
 import com.acme.dbo.txlog.Facade;
 import com.acme.dbo.txlog.SysoutCaptureAndAssertionAbility;
+import ooaddemo.domain.SeverityLevel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +10,8 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class LoggerTest implements SysoutCaptureAndAssertionAbility {
+    Facade facade = new Facade();
+
     //region given
     @Before
     public void setUpSystemOut() throws IOException {
@@ -23,94 +26,98 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     //endregion
 
 
-
     //TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogSequentIntegersAsSum() throws IOException {
-        //region when
-        Facade.log("str 1");
-        Facade.log(1);
-        Facade.log(2);
-        Facade.log("str 2");
-        Facade.log(0);
+
+        facade.log("str 1", SeverityLevel.WARNING);
+        facade.log(1, SeverityLevel.WARNING);
+        facade.log(2, SeverityLevel.WARNING);
+        facade.log("str 2", SeverityLevel.WARNING);
+        facade.log(0, SeverityLevel.WARNING);
+        facade.flush(SeverityLevel.WARNING);
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "3\n" +
-            "str 2\n" +
-            "0\n"
-        );
+        assertSysoutContains("str 1");
+        assertSysoutContains("3");
+        assertSysoutContains("str 2");
+        assertSysoutContains("0");
         //endregion
     }
+
 
     @Test
     public void shouldLogCorrectlyIntegerOverflowWhenSequentIntegers() {
         //region when
-        Facade.log("str 1");
-        Facade.log(10);
-        Facade.log(Integer.MAX_VALUE);
-        Facade.log("str 2");
-        Facade.log(0);
+        facade.log("str 1", SeverityLevel.WARNING);
+        facade.log(10, SeverityLevel.WARNING);
+        facade.log(Integer.MAX_VALUE, SeverityLevel.WARNING);
+        facade.log("str 2", SeverityLevel.WARNING);
+        facade.log(0, SeverityLevel.WARNING);
+        facade.flush(SeverityLevel.WARNING);
+
+
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "10\n" +
-            Integer.MAX_VALUE + "\n" +
-            "str 2\n" +
-            "0\n"
-        );
+
+        assertSysoutContains("str 1");
+        assertSysoutContains("10");
+        assertSysoutContains("" + Integer.MAX_VALUE);
+        assertSysoutContains("str 2");
+        assertSysoutContains("0");
+
         //endregion
     }
 
     @Test
     public void shouldLogCorrectlyByteOverflowWhenSequentBytes() {
         //region when
-        Facade.log("str 1");
-        Facade.log((byte)10);
-        Facade.log((byte)Byte.MAX_VALUE);
-        Facade.log("str 2");
-        Facade.log(0);
+        facade.log("str 1", SeverityLevel.WARNING);
+        facade.log((byte) 10, SeverityLevel.WARNING);
+        facade.log((byte) Byte.MAX_VALUE, SeverityLevel.WARNING);
+        facade.log("str 2", SeverityLevel.WARNING);
+        facade.log(0, SeverityLevel.WARNING);
+        facade.flush(SeverityLevel.WARNING);
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "10\n" +
-            Byte.MAX_VALUE + "\n" +
-            "str 2\n" +
-            "0\n"
-        );
+
+        assertSysoutContains("str 1");
+        assertSysoutContains("10");
+        assertSysoutContains("" + Byte.MAX_VALUE);
+        assertSysoutContains("str 2");
+        assertSysoutContains("0");
+
         //endregion
     }
 
     @Test
     public void shouldLogSameSubsequentStringsWithoutRepeat() throws IOException {
         //region when
-        Facade.log("str 1");
-        Facade.log("str 2");
-        Facade.log("str 2");
-        Facade.log(0);
-        Facade.log("str 2");
-        Facade.log("str 3");
-        Facade.log("str 3");
-        Facade.log("str 3");
+        facade.log("str 1", SeverityLevel.WARNING);
+        facade.log("str 2", SeverityLevel.WARNING);
+        facade.log("str 2", SeverityLevel.WARNING);
+        facade.log(0, SeverityLevel.WARNING);
+        facade.log("str 2", SeverityLevel.WARNING);
+        facade.log("str 3", SeverityLevel.WARNING);
+        facade.log("str 3", SeverityLevel.WARNING);
+        facade.log("str 3", SeverityLevel.WARNING);
+        facade.flush(SeverityLevel.WARNING);
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "str 2 (x2)\n" +
-            "0\n" +
-            "str 2\n" +
-            "str 3 (x3)\n"
-        );
+
+        assertSysoutContains("str 1");
+        assertSysoutContains("str 2 (x2)");
+        assertSysoutContains("0");
+        assertSysoutContains("str 2");
+        assertSysoutContains("str 3 (x3)");
+
         //endregion
     }
 
-    */
+
 }
--- src/test/java/com/acme/dbo/txlog/iteration03/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration03/LoggerTest.java
@@ -1,13 +1,18 @@
 package com.acme.dbo.txlog.iteration03;
 
+import com.acme.dbo.txlog.Facade;
 import com.acme.dbo.txlog.SysoutCaptureAndAssertionAbility;
+import ooaddemo.domain.SeverityLevel;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 public class LoggerTest implements SysoutCaptureAndAssertionAbility {
+    Facade facade = new Facade();
+
     //region given
     @Before
     public void setUpSystemOut() throws IOException {
@@ -21,18 +26,17 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     }
     //endregion
 
-    /*
-    TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogIntegersArray() throws IOException {
         //region when
-        Facade.log(new int[] {-1, 0, 1});
+        facade.log(new int[]{-1, 0, 1}, SeverityLevel.WARNING);
+        facade.flush(SeverityLevel.WARNING);
         //endregion
 
         //region then
         assertSysoutEquals(
-            "primitives array: {-1, 0, 1}\n"
+                "primitives array: {-1, 0, 1}\r\n"
         );
         //endregion
     }
@@ -40,40 +44,42 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     @Test
     public void shouldLogIntegersMatrix() throws IOException {
         //region when
-        Facade.log(new int[][] {{-1, 0, 1}, {1, 2, 3}, {-1, -2, -3}});
+        facade.log(new int[][]{{-1, 0, 1}, {1, 2, 3}, {-1, -2, -3}}, SeverityLevel.WARNING);
+        facade.flush(SeverityLevel.WARNING);
         //endregion
 
         //region then
         assertSysoutEquals(
-            "primitives matrix: {\n" +
-                "{-1, 0, 1}\n" +
-                "{1, 2, 3}\n" +
-                "{-1, -2, -3}\n" +
-            "}\n"
+                "primitives matrix: {\r\n" +
+                        "{-1, 0, 1}\r\n" +
+                        "{1, 2, 3}\r\n" +
+                        "{-1, -2, -3}\r\n" +
+                        "}\r\n"
         );
         //endregion
     }
-
-    @Test
-    public void shouldLogStringsWithOneMethodCall() throws IOException {
-        //region when
-        Facade.log("str1", "string 2", "str 3");
-        //endregion
-
-        //region then
-        assertSysoutContains("str1\nstring 2\nstr 3");
-        //endregion
-    }
-
-    @Test
-    public void shouldLogIntegersWithOneMethodCall() throws IOException {
-        //region when
-        Facade.log(-1, 0, 1, 3);
-        //endregion
-
-        //region then
-        assertSysoutContains("3");
-        //endregion
-    }
-    */
 }
+
+//    @Test
+//    public void shouldLogStringsWithOneMethodCall() throws IOException {
+//        //region when
+//        facade.log("str1", "string 2", "str 3");
+//        //endregion
+//
+//        //region then
+//        assertSysoutContains("str1\nstring 2\nstr 3");
+//        //endregion
+//    }
+
+//    @Test
+//    public void shouldLogIntegersWithOneMethodCall() throws IOException {
+//        //region when
+//        Facade.log(-1, 0, 1, 3);
+//        //endregion
+//
+//        //region then
+//        assertSysoutContains("3");
+//        //endregion
+//    }
+//    */
+//}
